be42371c401a9d1a68d3a352fe75f13bcbb5d2e2
lab36-ua <lab36@alu.ua.es>
2023-12-19 11:47:03 +0100

    Nombre

--- adivinanza.py
+++ adivinanza.py
@@ -9,14 +9,16 @@ def jugar_adivinanza():
     print("****************************************")
     print("Estoy pensando en un número entre 1 y 100. ¡Adivina!")
 
+    nombre = input("Introduce tu nombre: ")
+
     while True:
         intento_usuario = int(input("Tu suposición: "))
         intentos += 1
 
         if intento_usuario < numero_secreto:
-            print("Muy bajo. Intenta de nuevo.")
+            print(f"Muy bajo. Intenta de nuevo, {nombre}.")
         elif intento_usuario > numero_secreto:
-            print("Muy alto. Intenta de nuevo.")
+            print(f"Muy alto. Intenta de nuevo, {nombre}.")
         else:
             print("¡Error! ¡El juego siempre falla!")
             break
